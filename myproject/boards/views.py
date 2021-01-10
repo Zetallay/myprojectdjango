d9a2cd483c3aa1762785464e7d9ba4876200f0ae
Roberto <rober898248_@hotmail.com>
2021-01-10 10:47:24 -0700

    Advanced URLs

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Board
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
-
     boards = Board.objects.all()
-    boards_names = list()
+    return render(request,'home.html',{'boards':boards})
 
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html='<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
