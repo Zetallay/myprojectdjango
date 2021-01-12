02e9b267fde41596dbdacaeb466c1c754db21467
Roberto <rober898248_@hotmail.com>
2021-01-12 15:09:42 -0700

    Password Reset

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,15 +15,38 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path
-
+from django.contrib.auth import views as auth_views
 from boards import views
 from accounts import views as accounts_views
 
 urlpatterns = [
     path(r'', views.home, name='home'),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name = 'logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+
+
+    path('reset/',
+    auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt'
+    ),
+    name='password_reset'),
+    path('reset/<uidb64>/<token>/', 
+    auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+    name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+    name='password_reset_done'),
+    path('reset/complete/',
+    auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+    name='password_reset_complete'),
+    re_path(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+    name='password_change'),
+    re_path(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+    name='password_change_done'),
     
 ]
