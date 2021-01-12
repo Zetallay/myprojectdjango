2e007948c735af7dd1f703580d15a7b48112bdd2
Roberto <rober898248_@hotmail.com>
2021-01-11 21:16:07 -0700

    Sign in

--- myproject/accounts/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from django.core.urlresolvers import reverse
-from django.urls import resolve
-from .views import signup
-
-
-class SignUpTest(TestCase):
-    
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,7 +21,7 @@ from accounts import views as accounts_views
 
 urlpatterns = [
     path(r'', views.home, name='home'),
-    path(r'signup', accounts_views.signup, name='signup'),
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls)
