47d91ade5a1b520d537adbae143743be1e0cf20b
Roberto <rober898248_@hotmail.com>
2021-01-13 20:05:17 -0700

    View Update

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,7 +20,7 @@ from boards import views
 from accounts import views as accounts_views
 
 urlpatterns = [
-    path(r'', views.home, name='home'),
+    re_path(r'^$', views.BoardListView.as_view(), name='home'),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name = 'logout'),
@@ -48,5 +48,11 @@ urlpatterns = [
     name='password_change'),
     re_path(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
     name='password_change_done'),
+
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
+
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
+        views.PostUpdateView.as_view(), name='edit_post'),
     
 ]
